d2c71771ec9dbe2e337bb0933169ca0768347470
18521079 <18521079@gm.uit.edu.vn>
2021-05-08 09:56:53 +0700

    update content in home

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -6,37 +6,7 @@ import homecss from '../pages/homecss.css'
 
 function Navbar(){
     return(
-            /*<nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <div className="container">
-    <Link className="navbar-brand" to="/home">
-      <img src={logo} style={{width:'35px'}}/>
-      </Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span>
-      <i className="fas fa-align-justify" style={ {color:"white"}} ></i>
-      </span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 m-auto">
-        <li className="nav-item">
-          <Link className="nav-link text-uppercase text-white ml-5"  aria-current="page" to="/home">Home&nbsp;<i class="fas fa-home"></i></Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white text-uppercase ml-5" to="/news">New</Link>
-        </li>
-        
-        <li className="nav-item">
-          <Link className="nav-link text-uppercase ml-5 text-white" to="/contacts" tabindex="-1" aria-disabled="true">Contact</Link>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-*/
+            
 <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
 <div className="container">
 <Link className="navbar-brand" to="/home">
@@ -57,16 +27,17 @@ function Navbar(){
           Tâm lý
         </a>
         <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-          <li><Link className="dropdown-item" to="/search">Tra cứu</Link></li>
-          <li><Link className="dropdown-item" to="/books_management">Gặp chuyên gia</Link></li>
+          <li><Link className="dropdown-item" to="/search">Nhắn tin với chuyên gia</Link></li>
+          <li><Link className="dropdown-item" to="/books_management">Đặt lịch hẹn chuyên gia</Link></li>
         </ul>
       </li>
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/news">Bản tin</Link>
       </li>
+
       <li className="nav-item">
-        <a className="nav-link" href="#">Sức khỏe</a>
+        <Link className="nav-link" to="/search">Tra cứu</Link>
       </li>
 
       <li className="nav-item">
